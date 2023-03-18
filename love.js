f75c43619564a515caf0d87d90901c37e9023828
MellowWei <128209997+MellowWei@users.noreply.github.com>
2023-03-18 17:14:23 +0800

    Update love.js

--- love.js
+++ love.js
@@ -1,6 +1,6 @@
 var c = 0;
 
-var when = ["looked at me", "touch my hair and brush it", "always be my first reader", "wrote a song based on my melody", "tolerate my bad temper", "give me emotional support", "tell me I am great", "cooked a burnt egg for me", "are always positive"];
+var when = ["looked at me", "touch my hair and brush it", "always be my first reader", "wrote a song based on my melody", "tolerate my bad temper", "give me emotional support", "tell me I am great", "cooked a burnt egg for me", "are always positive", "write poems and read them for me", "are close to me", "walk with me randomly in the city", "remember what I like and dislike", "create romances and surprises", "are being vulnerable", "embrace my vulnerability", "are being nice and timely", "watch films with me even you don't really like them", "help me exploring my creativty", "tell me I could make everything happen", "are full of creative ideas", "show your great musician potential", "are very confident towards our relationship"];
 
 function shuffle(array) {
     array.sort(()=>Math.random() - 0.5);
