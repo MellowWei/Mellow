7ce8903fb74687e2821c48772f1f9d6217e51a7e
MellowWei <128209997+MellowWei@users.noreply.github.com>
2023-03-18 15:28:43 +0800

    Create love.js

---
+++ love.js
@@ -0,0 +1,34 @@
+var c = 0;
+
+var when = ["looked at me", "touch my hair and brush it", "always be my first reader", "wrote a song based on my melody", "tolerate my bad tempoer"];
+
+function shuffle(array) {
+    array.sort(()=>Math.random() - 0.5);
+}
+
+// shuffle on start
+$(function() {
+    shuffle(when);
+});
+
+$(document).click(function(e) {
+
+    if (e.button == 0) {
+
+        $("<p>" + when[c] + "</p>").hide().prependTo("#when-you").fadeIn(2000);
+
+        c++;
+
+        if (c == when.length) {
+
+            $("<p>love me too </p>").hide().prependTo("#when-you").fadeIn(2000);
+
+            $("body").css('background', '#000');
+            $("#g p").css('color', '#fff');
+
+            $(document).unbind("click");
+
+        }
+
+    }
+});
